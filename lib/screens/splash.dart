61334d55cbb22dd2168c9ce8c6e047dfe45ec5fe
Miheer Pandey <me.miheer@gmail.com>
2025-10-26 16:16:20 +0530

    added

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -25,7 +25,7 @@ class _SplashScreenState extends State<SplashScreen> {
     checkUpdates();
   }
 
-  final String appVersion = "3.0.1";
+  final String appVersion = "3.0.2";
 
   Future<void> _launchUrl(BuildContext context, String url) async {
     final Uri uri = Uri.parse(url);
